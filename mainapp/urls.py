c8d95d03f679bc27608bd7eab5c4a4134f02f870
Timur Minikhairov <matt_baker@mail.ru>
2023-01-22 16:35:32 +0000

    lesson_6 News CRUD and permissions

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,20 +1,101 @@
-from django.urls import path
+{% extends 'base.html' %}
 
-from mainapp import views
-from mainapp.apps import MainappConfig
+{% block title %}
+Новости
+{% endblock title %}
 
-app_name = MainappConfig.name
+{% block content %}
 
-urlpatterns = [
-    path("", views.MainPageView.as_view(), name="main_page"),
-    path("news/", views.NewsPageView.as_view(), name="news"),
-    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
-    path("courses/", views.CoursesListView.as_view(), name="courses"),
-    path(
-        "courses/<int:pk>/",
-        views.CoursesDetailView.as_view(),
-        name="courses_detail",
-    ),
-    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
-    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
-]
+{% if page_num %}
+<div class="card my-2">
+  <div class="card-body">
+    <p class="card-text">Текущая страница: {{ page_num }}</p>
+  </div>
+</div>
+{% endif %}
+
+<div class="row">
+  <div class="col-lg-8">
+
+    {% for item in object_list %}
+    <div class="card my-2">
+      <div class="card-body">
+        <h5 class="card-title">{{ item.title }}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">
+          {{ item.created|date:"Y-m-d h-i-s" }}
+        </h6>
+        <p class="card-text">{{ item.preambule }}</p>
+        <div class="row">
+          <div class="col">
+            <a href="{% url 'mainapp:news_detail' pk=item.pk %}"
+              class="card-link">Подробнее</a>
+          </div>
+          {% if perms.mainapp.change_news %}
+          <div class="col-1 text-center">
+            <a href="{% url 'mainapp:news_update' pk=item.pk %}">
+              <i class="far fa-edit"></i>
+            </a>
+          </div>
+          {% endif %}
+          {% if perms.mainapp.delete_news %}
+          <div class="col-1 text-center">
+            <a href="{% url 'mainapp:news_delete' pk=item.pk %}">
+              <i class="far fa-trash-alt"></i>
+            </a>
+          </div>
+          {% endif %}
+        </div>
+      </div>
+    </div>
+    {% endfor %}
+
+  </div>
+
+  <div class="col-lg-4">
+    <div class="card my-2 sticky-top">
+      <div class="card-header">
+        Фильтры
+      </div>
+      <div class="card-body">
+        <form action="">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1">Дата от...</span>
+            </div>
+            <input type="date" class="form-control" name="dateFrom">
+          </div>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="basic-addon1">Дата до...</span>
+            </div>
+            <input type="date" class="form-control" name="dateTo">
+          </div>
+
+          <button type="submit"
+            class="btn btn-primary btn-block">Фильтровать</button>
+        </form>
+      </div>
+    </div>
+
+    {% if perms.mainapp.add_news %}
+    <a class="btn btn-primary btn-block" role="button"
+      href="{% url 'mainapp:news_create' %}">Добавить новость</a>
+    {% endif %}
+
+  </div>
+
+</div>
+
+<div class="row justify-content-center align-items-center mt-3 mb-n3">
+  <nav aria-label="Page navigation example">
+    <ul class="pagination">
+      <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      {# <li class="page-item"><a class="page-link" href="#">3</a></li> #}
+      <li class="page-item"><a class="page-link" href="#">Next</a></li>
+    </ul>
+  </nav>
+</div>
+
+{% endblock content %}
